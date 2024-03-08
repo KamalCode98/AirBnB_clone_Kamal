c497edb148c806a6abf087b4b9f6b27433f34cbd
Kamal <malikleet1337@gmail.com>
2024-03-08 12:22:23 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -5,25 +5,28 @@ Module for BaseModel
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    def __init__(self, *args,**kwargs):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
-        for key, value in kwargs.items():
-            if key != "__class__":
-                setattr(self, key, value)
-            if key == "created_at" or key == "updated_at":
-                setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+
     def save(self):
-        self.upload_at = datetime.now
-        
+        self.updated_at = datetime.now()
+
     def to_dict(self):
-        dictionary = slef.__dict__.copy()
+        dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
